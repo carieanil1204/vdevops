04a93a288464bd2bf9b16b19eaf241661f945456
anil <anil@vitap.in>
2022-03-14 06:48:40 +0000

    second java file modfied'

--- second.java
+++ second.java
@@ -1,5 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println(" This is second Java file");  
+     System.out.println(" This is second Java file");
+     System.out.println("this  is an edied file ");  
     }  
 }  
